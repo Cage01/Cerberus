570b30647254ea46a4ccda675704fe7ab11f0b19
Cage01 <mason.p.richardson@gmail.com>
2022-03-31 20:39:37 -0700

    Breaking things down into components that can be shared among other classes

--- Source/Cerberus/Core/Private/Weapons/WeaponBase.cpp
+++ Source/Cerberus/Core/Private/Weapons/WeaponBase.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Cerberus/Core/Public/Weapons/WeaponBase.h"
+#include "Cerberus/Core/Public/Combat/AttackComponent.h"
+#include "Cerberus/Dev/Public/Projectile.h"
 
 
 // Sets default values
@@ -9,88 +11,48 @@ AWeaponBase::AWeaponBase()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	
+	AttackComponent = CreateDefaultSubobject<UAttackComponent>(TEXT("AttackComponent"));
+	AttackComponent->ReplicatedAttack.BindUObject(this, &AWeaponBase::ServerOnAttack);
+	AddOwnedComponent(AttackComponent);
+
+	
 }
 
 // Called when the game starts or when spawned
 void AWeaponBase::BeginPlay()
 {
 	Super::BeginPlay();
-
-	//Set Defaults
-	MaxAmmo = 250;
-	ClipSize = 30;
-	FireRate = 0.1f;
-	Range = 100.0f; //Probably will represent "Ideal Range" where further than that the damage begins to drop off
-	Type = Hitscan;
 	
-	Damage = 1.0f;
-	DamageType = UDamageType::StaticClass();
-
-	bIsFiringWeapon = false;
 }
 
-void AWeaponBase::StartFire()
+
+
+void AWeaponBase::ServerOnAttack_Implementation()
 {
-	if (!bIsFiringWeapon)
-	{
-		bIsFiringWeapon = true;
-		UWorld* World = GetWorld();
-		// StopFire is called when the time with the length of FireRate finishes -- bIsFiringWeapon will be set to false automatically
-		//@TODO : this does not account for automatic firing / Seems to be good for semi-automatic weapons
-		World->GetTimerManager().SetTimer(FiringTimer, this, &AWeaponBase::StopFire, FireRate, false);
-		
-		ServerOnFire();
-	}
+	//Hitscan attack type
+	if (!bIsHitscan)
+		SpawnProjectile();
+	else
+		PreformHitscan();
+
 }
 
-void AWeaponBase::StopFire()
+void AWeaponBase::PreformHitscan()
 {
-	bIsFiringWeapon = false;
-}
-
-void AWeaponBase::ServerOnFire_Implementation()
-{
-	bool bSuccess = false;
 	
-	switch (Type)
-	{
-	case Hitscan:
-		bSuccess = FireHitscan();
-		break;
-	case Projectile:
-		bSuccess = FireProjectile();
-		break;
-	case Melee:
-		bSuccess = FireMelee();
-		break;
-	}
-
-	//Not sure what to do with bSuccess yet.
 }
 
-
-bool AWeaponBase::FireHitscan()
+void AWeaponBase::SpawnProjectile()
 {
-	return true;
-}
+	FVector spawnLocation = GetActorLocation() + (GetActorRotation().Vector() * 100.0f) + (GetActorUpVector() * 50.0f);
+	FRotator spawnRotation = GetActorRotation();
 
-bool AWeaponBase::FireProjectile()
-{
-	// FVector spawnLocation = GetActorLocation() + (GetControlRotation().Vector() * 100.0f) + (GetActorUpVector() * 50.0f);
-	// FRotator spawnRotation = GetControlRotation();
-	//
-	// FActorSpawnParameters spawnParameters;
-	// spawnParameters.Instigator = GetInstigator();
-	// spawnParameters.Owner = this;
-	//
-	// GetWorld()->SpawnActor<AProjectile>(spawnLocation, spawnRotation, spawnParameters);
+	FActorSpawnParameters spawnParameters;
+	spawnParameters.Instigator = GetInstigator();
+	spawnParameters.Owner = this;
 
-	return true;
-}
-
-bool AWeaponBase::FireMelee()
-{
-	return true;
+	GetWorld()->SpawnActor<AProjectile>(spawnLocation,spawnRotation, spawnParameters);
 }
 
 // Called every frame
--- Source/Cerberus/Core/Public/Weapons/WeaponBase.h
+++ Source/Cerberus/Core/Public/Weapons/WeaponBase.h
@@ -3,15 +3,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Cerberus/Dev/Public/Projectile.h"
 #include "GameFramework/Actor.h"
 #include "WeaponBase.generated.h"
 
-UENUM(BlueprintType)
-enum WeaponType
+USTRUCT()
+struct FAmmoAmountChangedPayload
 {
-	Hitscan		UMETA(DisplayName = "Hitscan"),
-	Projectile	UMETA(DisplayName = "Projectile"),
-	Melee	UMETA(DisplayName = "Melee"),
+	GENERATED_BODY()
+
+	int32 CurrentAmmoInClip;
+	int32 ClipCapacity;
+	int32 CurrentAmmo;
 };
 
 UCLASS()
@@ -28,55 +31,34 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-
-
-	/** a timer handle used for providing the fire rate delay in-between spawns.*/
-	FTimerHandle FiringTimer;
-
-	/** Function for beginning weapon fire. */
-	UFUNCTION(BlueprintCallable, Category="Weapon")
-	void StartFire();
-
-	/** Function for ending weapon fire. */
-	UFUNCTION(BlueprintCallable, Category="Weapon")
-	void StopFire();
-
 	/** Server function for spawning projectiles. */
 	UFUNCTION(Server, Reliable)
-	void ServerOnFire();
+	virtual void ServerOnAttack();
 
-	/** Delay between shots in seconds. Also prevents an overflow of server functions from binding SpawnProjectile directly to input.*/
-	UPROPERTY(EditDefaultsOnly, Category="Weapon")
-	float FireRate;
-
-	/** If true, we are firing projectiles */ //@TODO : options for melee and non-projectile type weapons 
-	bool bIsFiringWeapon;
+	void PreformHitscan();
+	virtual void SpawnProjectile();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UAttackComponent* AttackComponent;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Ammo")
 	int32 MaxAmmo;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Ammo")
-	int32 ClipSize;
 
 	// The damage type and damage that will be done by this weapon
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Damage")
 	TSubclassOf<class UDamageType> DamageType;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	bool bIsHitscan;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	float Range;
+	
 	// The damage dealt by this weapon
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Damage")
 	float Damage;
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Damage")
-	float Range;
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
-	TEnumAsByte<WeaponType> Type;
 
-	//Fire modes for each weapon type. Should return if hit was successful or not.
-	bool FireHitscan();
-	bool FireProjectile();
-	bool FireMelee();
-
+	
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
