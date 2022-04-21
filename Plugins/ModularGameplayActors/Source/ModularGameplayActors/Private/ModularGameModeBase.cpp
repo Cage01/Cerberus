e903f57d5cd7a36674f42b9ef7433798b85a3940
Cage01 <mason.p.richardson@gmail.com>
2022-04-20 23:31:32 -0700

    Porting over the rest of the code from this plugin.
     I will be using it extend from to create a new GameMode class

---
+++ Plugins/ModularGameplayActors/Source/ModularGameplayActors/Private/ModularGameModeBase.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ModularGameModeBase.h"
+
+#include "ModularGameState.h"
+#include "ModularPawn.h"
+#include "ModularPlayerController.h"
+#include "ModularPlayerState.h"
+
+AModularGameModeBase::AModularGameModeBase(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	GameStateClass = AModularGameStateBase::StaticClass();
+	PlayerControllerClass = AModularPlayerController::StaticClass();
+	PlayerStateClass = AModularPlayerState::StaticClass();
+	DefaultPawnClass = AModularPawn::StaticClass();
+}
+
+AModularGameMode::AModularGameMode(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	GameStateClass = AModularGameState::StaticClass();
+	PlayerControllerClass = AModularPlayerController::StaticClass();
+	PlayerStateClass = AModularPlayerState::StaticClass();
+	DefaultPawnClass = AModularPawn::StaticClass();
+}
